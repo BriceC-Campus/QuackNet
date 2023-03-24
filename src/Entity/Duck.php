c9394ea3070cd275ebc481089a22783de5af3f46
BriceC-Campus <brice.chaves@le-campus-numerique.fr>
2023-03-24 15:43:13 +0100

    Working state login and registration

--- src/Entity/Duck.php
+++ src/Entity/Duck.php
@@ -11,6 +11,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: DuckRepository::class)]
 #[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
 class Duck implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
